502c6ce36c7099432aacebb4e0b4c978d66ae7c8
LuisLars89 <tacuboo@hotmail.com>
2023-02-26 18:38:38 -0600

    ALL

---
+++ src/assets/main.js
@@ -0,0 +1,52 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCLYZ9Q-Cas_PtYqfiL6Tqyg&part=snippet%2Cid&order=date&maxResults=8';
+
+
+
+const content = null || document.getElementById('content');
+
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Key': 'c87616ec71mshba0f4dc04db938cp1fa974jsn41c98b6b3245'
+  }
+};
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map(video => `
+    <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+      </a>
+    `).slice(0,4).join('')}
+
+    `;
+
+    content.innerHTML = view;
+
+  } catch (error) {
+    console.log(error);
+
+  }
+})();
